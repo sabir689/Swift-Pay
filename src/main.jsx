f2445377d2ba063be1e526a29a683cf82a37579f
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-01-16 20:45:13 +0600

    add registation page and design it

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayOut from "./Components/Layout/MainLAyOut.jsx";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayOut></MainLayOut>,
-  },
-]);
+import {RouterProvider } from "react-router-dom";
+import { router } from "./routes/Route.jsx";
+import { HelmetProvider } from 'react-helmet-async';
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <MainLayOut></MainLayOut>,
+//   },
+// ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="font-poppins">
+  <HelmetProvider>
+  <div className="font-poppins">
       <RouterProvider router={router} />
     </div>
+  </HelmetProvider>
   </React.StrictMode>
 );
--- src/pages/authentication/Registration.jsx
+++ src/pages/authentication/Registration.jsx
@@ -1,14 +1,85 @@
-import React from 'react'
-import Container from '../../Components/Shared/Container'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { PiEyeSlash, PiEye } from 'react-icons/pi'
+import { Helmet } from "react-helmet-async"
 
-const Registration = () => {
+const Register = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [registrationError, setRegistrationError] = useState('')
+  const validation = {
+    'capital': /[A-Z]/,
+    'spacelSymble': /[\W_]+/,
+    'number': /\d/
+  };
+  const registration = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const confirmEmail = form.re-email.value;
+    const password = form.password.value
+    const confirmPassword = form.confirmPassword.value
+
+    setRegistrationError('')
+    if (password.length < 6) {
+      setRegistrationError('password must be at least 6 characters long.')
+      return
+    } else if (!validation.capital.test(password)) {
+      setRegistrationError('At least one uppercase letter')
+      return
+    } else if (!validation.spacelSymble.test(password)) {
+      setRegistrationError('At least one special character')
+      return
+    } else if(!validation.number.test(password)){
+      setRegistrationError('Password at least one number.')
+      return
+    }
+  }
+
   return (
-    <Container>
-        <div>
-            
+    <div className='bg-sectionbg py-14'>
+      <Helmet>
+        <title> Regitation </title>
+      </Helmet>
+      <div className='container md:w-10/12/ lg:w-2/4 mx-auto px-4'>
+        <div className='bg-white px-6 py-10 rounded-lg'>
+          <h1 className='text-2xl font-medium text-black1 mb-5'>Create your Account</h1>
+          <form onSubmit={registration}>
+            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-5 block'>Full Name</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='Name' name='name' type='text' />
+
+            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-5 block'>Gmail</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='gmail' name='email' type='email' required />
+
+            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-5 block'>Confirm Gmail</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='Write same gmail' name='re-email' type='email'/>
+
+            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-4 block'>Password</label>
+            <div className='h-16 relative'>
+              <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='Password' name='password' type={showPassword ? 'text' : 'password'} required />
+              <span className='absolute top-4 right-5 text-2xl' onClick={() => setShowPassword(!showPassword)}>
+                {
+                  showPassword ? <PiEye> </PiEye> : <PiEyeSlash> </PiEyeSlash>
+                }
+              </span>
+            </div>
+            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-4 block'>Confirm Password</label>
+            <div className='h-16 relative'>
+              <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='write same password' name='confirmPassword' type={showPassword ? 'text' : 'password'} required />
+            </div>
+            <div>
+              {
+                registrationError && <p className='text-lg text-red-400 font-normal'> {registrationError} </p>
+              }
+            </div>
+            <input type="submit" value="Register" className='text-lg px-10 py-[14px] bg-neutral-300 rounded-lg text-black mt-5 w-full inline-block duration-300' />
+            <h1 className='text-center text-lg font-normal text-black1  my-3'> Don't you have an account?
+              <Link to='/login' className='text-lg hover:underline hover:text-hover duration-300 ml-2 font-normal text-primery inline-block'>Login</Link>
+            </h1>
+          </form>
         </div>
-    </Container>
+      </div>
+    </div>
   )
 }
-
-export default Registration
+export default Register
