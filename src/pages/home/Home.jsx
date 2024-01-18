4b14dbeb8565e70e5866eb7eacaa4ada7d53ee23
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-01-18 21:26:46 +0600

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
 import Banner from "./Banner";
-import PlatformFeatures from "./platformFeature/PlatformFeatures";
 
 const Home = () => {
   return (
     // <div>Home</div>
     <div className="mt-20 mb-10">
       <Banner></Banner>
-      <PlatformFeatures/>
     </div>
-    
+
   );
 };
 
