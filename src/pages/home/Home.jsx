8e97250a53ffd605672a34b85874bbc28bbbffa9
Sahariar Hasan <138358352+sabir689@users.noreply.github.com>
2024-01-18 21:07:29 +0600

    SLider Update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "./Banner";
+import ServicesSlider from "./ServicesSlider";
+
 
 const Home = () => {
   return (
     // <div>Home</div>
     <div className="mt-20 mb-10">
       <Banner></Banner>
+      <ServicesSlider></ServicesSlider>
     </div>
   );
 };
