9039335cb85f1899c6370ae8523a6ef644a4c9b3
alzabid <alzabid.ashik@gmail.com>
2024-01-19 20:30:03 +0600

    deployment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,6 @@ import WhatIsNew from "./WhatIsNew";
 
 const Home = () => {
   return (
-    // <div>Home</div>
-<<<<<<< HEAD
     <div className="">
       <div className="mt-20 mb-10">
         <Banner></Banner>
@@ -15,14 +13,9 @@ const Home = () => {
       <div className="max-w-screen-2xl mx-auto mt-20 mb-10">
         <WhatIsNew></WhatIsNew>
       </div>
-=======
-    <div className="mt-20 mb-10">
-      <Banner></Banner>
->>>>>>> cd4584e3c22552c5ff4edeb6f0f9f5d2d5f8031d
     </div>
 
   );
 };
 
 export default Home;
-//
