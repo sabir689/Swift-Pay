6fae518b66ce274e2c6dcdfb6531577084f2f751
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-01-23 22:14:21 +0600

    update login and Registration page

--- src/pages/authentication/Registration.jsx
+++ src/pages/authentication/Registration.jsx
@@ -21,14 +21,9 @@ const Register = () => {
     const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value
-    const confirmPassword = form.confirmPassword.value
     setRegistrationError('')
     if (password.length < 6) {
       setRegistrationError('password must be at least 6 characters long.')
-      return
-    }
-    else if (password !== confirmPassword) {
-      setRegistrationError('Password and confirm password do not match.')
       return
     }
     else if (!validation.capital.test(password)) {
@@ -64,41 +59,47 @@ const Register = () => {
       </Helmet>
       <div className='container md:w-10/12/ lg:w-2/4 mx-auto px-4'>
         <div className='bg-white px-6 py-10 rounded-lg'>
+        <h1 className='text-3xl font-semibold text-black mb-1 ml-1  block'>Create an account</h1>
+            <h1 className=' text-lg font-normal text-neutral-600 '>Already have an account?
+              <Link to='/login' className='text-lg hover:underline text-cyan-500  duration-300 ml-2 font-normal text-primery inline-block'>Login</Link>
+            </h1>
           <form onSubmit={registration}>
-            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-5 block'>Full Name</label>
-            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='Name' name='name' type='text' />
+           <div className='flex flex-col md:flex-row gap-3'>
+            <div className='flex-1'>
+            <label className='text-base font-normal text-neutral-600 mb-1 ml-1 mt-3 block'>First Name</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-3 px-3 w-full' placeholder='Name' name='name' type='text' />
+            </div>
 
-            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-5 block'>Gmail</label>
-            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='gmail' name='email' type='email' required />
+            <div className='flex-1'>
+            <label className='text-base font-normal text-neutral-600 mb-1 ml-1 mt-3 block'>Last Name</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-3 px-3 w-full' placeholder='Name' name='name' type='text' />
+            </div>
+           </div>
 
-            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-4 block'>Password</label>
-            <div className='h-16 relative'>
-              <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='Password' name='password' type={showPassword ? 'text' : 'password'} required />
+            <label className='text-base font-normal text-neutral-600 mb-1 ml-1 mt-3 block'>Gmail</label>
+            <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-3 px-3 w-full' placeholder='gmail' name='email' type='email' required />
+
+            <label className='text-base font-normal text-neutral-600 mb-1 ml-1 mt-4 block'>Password</label>
+            <div className='h-14 relative'>
+              <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-3 px-3 w-full' placeholder='Password' name='password' type={showPassword ? 'text' : 'password'} required />
               <span className='absolute top-4 right-5 text-2xl' onClick={() => setShowPassword(!showPassword)}>
                 {
                   showPassword ? <PiEye> </PiEye> : <PiEyeSlash> </PiEyeSlash>
                 }
               </span>
             </div>
-            <label className='text-lg font-normal text-black1 mb-1 ml-1 mt-4 block'>Confirm Password</label>
-            <div className='h-16 relative'>
-              <input className='text-base placeholder:text-textColor bg-sectionbg rounded-lg border duration-300 focus:bg-white focus:shadow-sm focus:border focus:border-primery outline-none py-4 px-5 w-full' placeholder='write same password' name='confirmPassword' type={showPassword ? 'text' : 'password'} required />
-            </div>
+            
             <div>
               {
                 registrationError && <p className='text-lg text-red-400 font-normal'> {registrationError} </p>
               }
             </div> 
             <button type="submit"
-          className='text-lg px-10 py-[14px] bg-neutral-300 rounded-lg text-black mt-5 w-full inline-block duration-300'>
+          className='text-lg font-medium px-10 py-[14px] bg-neutral-300 rounded-lg text-black mt-5 w-full inline-block duration-300'>
             {
-              loading?<ImSpinner9 className="animate-spin text-center inline-block"/> : "Register"
+              loading?<ImSpinner9 className="animate-spin text-center inline-block"/> : "Registration"
             }
           </button>
-
-            <h1 className='text-center text-lg font-normal text-black1  my-3'>Already have an account?
-              <Link to='/login' className='text-lg hover:underline hover:text-hover duration-300 ml-2 font-normal text-primery inline-block'>Login</Link>
-            </h1>
           </form>
         </div>
       </div>
