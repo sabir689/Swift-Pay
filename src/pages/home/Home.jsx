bd460356d1d7fc3d49bd66c61e6ca5a804ed0279
Mohammad Rayyan <rayyan3379@gmail.com>
2024-01-25 21:58:48 +0600

    checking

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,26 +1,23 @@
 import Banner from "./Banner";
+import Business from "./Business";
+import ServicesSlider from "./ServicesSlider";
 import WhatIsNew from "./WhatIsNew";
-
-
 
 const Home = () => {
   return (
     // <div>Home</div>
-<<<<<<< HEAD
     <div className="">
       <div className="mt-20 mb-10">
         <Banner></Banner>
       </div>
-
+      <ServicesSlider></ServicesSlider>
+      <div className=" mt-10">
+        <Business></Business>
+      </div>
       <div className="max-w-screen-2xl mx-auto mt-20 mb-10">
         <WhatIsNew></WhatIsNew>
       </div>
-=======
-    <div className="mt-20 mb-10">
-      <Banner></Banner>
->>>>>>> cd4584e3c22552c5ff4edeb6f0f9f5d2d5f8031d
     </div>
-
   );
 };
 
