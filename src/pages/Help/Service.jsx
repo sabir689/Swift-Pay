8ac53794efded626c91aaf20dadc5231d17dff04
alzabid <alzabid.ashik@gmail.com>
2024-01-26 17:28:53 +0600

    Respnsived

--- src/pages/Help/Service.jsx
+++ src/pages/Help/Service.jsx
@@ -4,8 +4,9 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 
 const Service = () => {
   return (
-    <div className=" bg-cyan-50 my-10">
-      <div className=" w-[1100px] h-[400px] mx-auto flex gap-20 items-center">
+    <div className=" bg-cyan-50 px-8 my-10">
+      <div className=" max-w-[1100px] mx-auto flex flex-col lg:flex-row lg:gap-20 items-center">
+        {/* img */}
         <div>
           <img
             className="w-[800px]"
@@ -13,23 +14,24 @@ const Service = () => {
             alt=""
           />
         </div>
+        {/* text */}
         <div>
           <h1 className="text-4xl font-semibold">24/7 at your service:</h1>
           <p className="py-6">
-            Dedicated bKash representatives are available 24/7 via 16247, Live
+            Dedicated swift-pay representatives are available 24/7 via 16247, Live
             Chat & Facebook. You can also take e-Appointments or email at
-            support@bkash.com
+            support@swift.com
           </p>
 
           <div className="">
             <button className="text-xl btn px-8 md:px-10 hover:text-white text-gray-800 bg-white border-cyan-400 border-2 hover:bg-cyan-500">
               <BiPhoneCall className="text-cyan-500 text-2xl " /> 16247
             </button>
-            <button className=" ml-5 text-xl btn px-8 md:px-10 hover:text-white text-gray-800 bg-white border-cyan-400 border-2 hover:bg-cyan-500">
+            <button className=" lg:ml-5 text-xl btn px-8 md:px-10 hover:text-white text-gray-800 bg-white border-cyan-400 border-2 hover:bg-cyan-500">
               <FaRegCalendarAlt className="text-cyan-500  text-2xl " />{" "}
               e-Appointment
             </button>
-            <button className="mt-4 text-xl  btn px-8 md:px-10 hover:text-white text-gray-800 bg-white border-cyan-400 border-2 hover:bg-cyan-500">
+            <button className="lg:mt-4 text-xl  btn px-8 md:px-10 hover:text-white text-gray-800 bg-white border-cyan-400 border-2 hover:bg-cyan-500">
               <IoChatbubbleEllipsesOutline className="text-cyan-500  text-2xl " />
               Live Chat
             </button>
