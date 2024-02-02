7a9018a108ef1c9ebe0ca901e33cb1f8277727a8
alzabid <alzabid.ashik@gmail.com>
2024-02-02 18:02:13 +0600

    added delete functionalities

--- Swift-pay-client/src/Dashboard/Saved.jsx
+++ Swift-pay-client/src/Dashboard/Saved.jsx
@@ -3,32 +3,67 @@ import { CiLocationOn } from "react-icons/ci";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
+import { MdAutoDelete, MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
 const Saved = () => {
-   const { user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const axiosPublic = useAxiosPublic();
 
-  const { data: bookmarks = [] } = useQuery({
+  const { data: bookmarks = [], refetch } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
       const res = await axiosPublic.get(`/api/bookmarks?email=${user.email}`);
       return res.data;
     },
   });
-  
+  console.log(bookmarks);
+
+  const handleDelete = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosPublic.delete(`/api/bookmarks/${_id}`).then((res) => {
+          console.log(res.data);
+          if (res.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: "Item has been deleted.",
+              icon: "success",
+            });
+            refetch();
+          }
+        });
+      }
+    });
+  };
 
   return (
     <div>
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-4">
         {bookmarks.map((product) => (
           <div
-            key={product.id}
+            key={product._id}
             className="w-72 mb-5 bg-transparent shadow rounded-lg border border-transparent hover:border-[#49108B]  cursor-pointer"
           >
             <div
-              className="h-48 w-full bg-gray-200 flex flex-col justify-between rounded-tl-lg rounded-tr-lg p-4 bg-cover bg-center"
+              className="relative h-48 w-full bg-gray-200 flex flex-col justify-between rounded-tl-lg rounded-tr-lg p-4 bg-cover bg-center"
               style={{ backgroundImage: `url(${product?.image})` }}
-            ></div>
+            >
+              <div
+                onClick={() => handleDelete(product._id)}
+                className="absolute top-2 right-2 w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center text-blue-400"
+              >
+                <MdDelete className="text-2xl text-red-700" />
+              </div>
+            </div>
             <div className="p-4">
               <div className="">
                 <h1 className="text-gray-600 font-medium">
--- swift-pay-server/index.js
+++ swift-pay-server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
@@ -75,6 +75,14 @@ async function run() {
         query = { email: req.query.email };
       }
       const result = await bookmarkCollection.find(query).toArray();
+      res.send(result);
+    });
+
+   
+    app.delete("/api/bookmarks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookmarkCollection.deleteOne(query);
       res.send(result);
     });
 
