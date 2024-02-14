6a754a551e2cd7d2c95dd1fe07c796937dce0336
fakeistiak <istiakahmedb100.com>
2024-02-14 14:54:04 +0600

    Offer and Offer detail page design changed

--- Swift-pay-client/src/pages/home/Offer.jsx
+++ Swift-pay-client/src/pages/home/Offer.jsx
@@ -1,22 +1,20 @@
-
 import { Link } from "react-router-dom";
 import Container from "../../Components/Shared/Container";
 import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 const Offer = () => {
+  const axiosPublic = useAxiosPublic();
   const { data: Offers = [] } = useQuery({
     queryKey: ["offers"],
     queryFn: async () => {
-      const res = await useAxiosPublic.get("/api/offers");
+      const res = await axiosPublic.get("/api/offers");
       return res.data;
     },
   });
   return (
     <Container>
       <div>
-
-      
-        <div className="container mx-auto">
+        <div className="container mx-auto lg:py-20 py-4">
           <div className="items-center lg:flex">
             <div className="">
               <div className="text-center">
@@ -43,47 +41,9 @@ const Offer = () => {
             <div className="flex flex-col items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2 gap-4">
               <img
                 className="w-full lg:h-[500px] lg:max-w-3xl rounded-lg object-cover"
-                src="https://i.ibb.co/gvXVQnT/151914-OUR8-OJ-83.jpg"
+                src="https://i.ibb.co/hWSKH4s/top-view-black-friday-sales-elements-composition-1.jpg"
                 alt="Catalogue-pana.svg"
               />
-              <div className="flex gap-5">
-                <div
-                  className="card shadow-lg"
-                  data-aos="zoom-out"
-                  data-aos-delay="100"
-                >
-                  <img
-                    className="w-full h-60 lg:max-w-xl rounded-lg rounded-bl-none rounded-br-none object-cover object-center"
-                    src="https://i.ibb.co/YZYKPKQ/9781383.jpg"
-                  />
-                  <div className="card-content p-3">
-                    <h3 className="text-lg font-medium text-[#212427]">
-                      Win Prizes with SwiftPay
-                    </h3>
-                    <p className="text-sm text-gray-400">
-                      Engage with SwiftPay to get the giveaway prizes every day
-                    </p>
-                  </div>
-                </div>
-                <div
-                  className="card shadow-lg"
-                  data-aos="zoom-out"
-                  data-aos-delay="200"
-                >
-                  <img
-                    className="w-full h-60 lg:max-w-xl rounded-bl-none rounded-br-none  rounded-lg object-cover object-center"
-                    src="https://i.ibb.co/VwMY2vd/tamanna-rumee-m-Iqy-Yp-SNq3o-unsplash-1.jpg"
-                  />
-                  <div className="card-content p-3">
-                    <h3 className="font-medium text-lg text-[#212427]">
-                      Black Friday Sale
-                    </h3>
-                    <p className="text-sm text-gray-400">
-                      Always updated with SwiftPAY to get the sale every friday
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
         </div>
